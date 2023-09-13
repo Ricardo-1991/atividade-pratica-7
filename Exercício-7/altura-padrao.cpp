9325817d09b53c3bd52e1b061d0ea6542bfe3de0
Ricardo <ricardo.dev1991@gmail.com>
2023-09-13 18:55:51 -0300

    Adicionando exercicio 7

---
+++ Exercício-7/altura-padrao.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int height;
+    cout << "Informe a altura do triangulo de caracteres: " << endl;
+    cin >> height;
+    return 0;
+}
