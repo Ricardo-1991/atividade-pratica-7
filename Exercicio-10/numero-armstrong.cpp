410510c36af8277120dd7da19e7477feb9249db8
Ricardo <ricardo.dev1991@gmail.com>
2023-09-17 13:19:09 -0300

    Nova versão retificada alguns bugs

--- Exercicio-10/numero-armstrong.cpp
+++ Exercicio-10/numero-armstrong.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -9,7 +10,7 @@ int main () {
     número armstrong. Um número armstrong (ou narcisista) é um número que é igual à
     soma de seus próprios dígitos elevados à quantidade de dígitos. Por exemplo, 153 é
     um número armstrong porque 1³ + 5³ + 3³ = 153.*/
-    int number, digit, sum = 0;
+    int number, digit = 0, sum = 0, multi = 1;
 
     cout << "Digite um numero inteiro para saber se ele eh um numero Armstrong: ";
     cin >> number;
@@ -21,14 +22,18 @@ int main () {
 
    while(number != 0) {
         digit = number % 10;
-        sum += pow(digit, numberOfDigits);
+        for(int i = 0; i < numberOfDigits; i++) {
+            multi *= digit;
+        }
+        sum += multi;
+        multi = 1;
         number /= 10;
-    } 
+    }
 
     string message = 
-    (inputNumber == sum ? " é um número Armstrong! O resultado da soma das potências elevado ao total de digitos do numero eh: " 
+    (inputNumber == sum ? " eh um nusmero Armstrong! O resultado da soma das potencias elevado ao total de digitos do numero eh: " 
     : 
-    " não eh um número Armstrong. O resultado da soma das potências elevado ao total de digitos do numero eh: ");
+    " nao eh um numero Armstrong. O resultado da soma das potencias elevado ao total de digitos do numero eh: ");
     
     cout << inputNumber << message << sum << endl;
 
--- Exercicio-2/palindromo.cpp
+++ Exercicio-2/palindromo.cpp
@@ -11,9 +11,9 @@ int main () {
  isPalindromeNumber = number;
 
     while(number != 0) {
-        newDigit = number % 10;
-        reverseNumber = reverseNumber * 10 + newDigit;
-        number /= 10;
+        newDigit = number % 10; // Obtem o último dígito do numero, que é o resto da divisão por 10;
+        reverseNumber = reverseNumber * 10 + newDigit; // ao multiplicar por 10, o numero anda uma casa a esquerda e adiciona o digito
+        number /= 10; // ao dividir por 10, o ultimo digito some, dando inicio ao um novo ciclo do loop.    
     }
 
     if(isPalindromeNumber == reverseNumber) {
